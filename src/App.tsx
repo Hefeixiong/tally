ae924b8640adea36f7be6e015219244d4382685c
hefeixiong <1074535830@qq.com>
2022-07-06 16:36:22 +0800

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,84 @@
 import React from 'react';
+import { 
+  HashRouter as Router,
+  Routes,
+  Route,
+  Link,
+  // Redirect
+ } from 'react-router-dom';
+import styled from 'styled-components';
 
+
+const Wrapper = styled.div`
+  height: 100hv;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Nav = styled.div`
+  border: 1px solid  blue;
+  > ul {
+    dispaly: flex;
+    > li {
+      width: 33.333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div>Hello</div>
-  )
+    <Router>
+       <Wrapper>
+         <Main>
+          <Routes>
+             <Route path="/tags" element={<Tags />} />
+             <Route path="/money"  element={<Money/>} />
+             <Route path="/statistics" element={<Statistics/>} />
+             <Route path="*" element={<NoMatch/>} />
+          </Routes>
+         </Main>
+         <Nav>
+           <ul>
+             <li>
+               <Link to="/tags">标签页</Link>
+             </li>
+             <li>
+               <Link to="/money">记账页</Link>
+             </li>
+             <li>
+               <Link to="/statistics">统计页</Link>
+             </li>
+           </ul>
+         </Nav>
+       </Wrapper>
+     </Router>
+   );
 }
 
+function NoMatch() {
+  return (
+    <div>页面不存在,你丫输错地址了吧！</div>
+  );
+}
+
+function Statistics() {
+  return (
+    <h2>统计页</h2>
+  );
+}
+
+function Tags() {
+  return <h2>标签页</h2>
+}
+
+function Money() {
+  return <h2>记账页</h2>
+}
 export default App;
