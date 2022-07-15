206631bb898b6a3f76810986dba50dbbc446cac1
hefeixiong <1074535830@qq.com>
2022-07-15 10:01:24 +0800

    完成CardSection样式

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -2,30 +2,7 @@ import React from "react";
 import { Toast } from "antd-mobile";
 import Layout from "components/Layout";
 import { useTags } from "hooks/useTags";
-import styled from "styled-components";
-
-const Card = styled.div`
-  font-size: 16px;
-  background: white;
-  display: flex;
-  margin: 8px 16px;
-  padding: 0 12px;
-  border-radius: 8px;
-  flex-direction: column;
-  > .title {
-    padding: 12px 0;
-    display: flex;
-    align-items: center;
-    border-bottom: 0.5px solid #eee;
-  }
-  > .content {
-    padding: 12px 0;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-  }
-`;
+import { CardSection } from "./statistic/card";
 
 function Statistics() {
   const { tags } = useTags();
@@ -34,15 +11,7 @@ function Statistics() {
   };
   return (
     <Layout className="统计">
-      <Card onClick={onClick}>
-        <div className="title">
-          <div>本月收支</div>
-        </div>
-        <div className="content">
-          <div>支出: ¥ 1222</div>
-          <div>收入: ¥1300</div>
-        </div>
-      </Card>
+      <CardSection />
     </Layout>
   );
 }
