a773af6bf8393d5ba1195b38a20cbe3ceb01cbd5
hefeixiong <1074535830@qq.com>
2022-07-18 17:15:57 +0800

    add useRecords

---
+++ src/hooks/useRecords.tsx
@@ -0,0 +1,38 @@
+import { Toast } from "antd-mobile";
+import { useEffect, useState } from "react";
+import { useUpdate } from "./useUpdate";
+
+export type RecordItem = {
+  tagIds: number[];
+  note: string;
+  amount: number;
+  category: "+" | "-";
+  createdAt: string;
+};
+
+type newRecordItem = Omit<RecordItem, "createdAt">;
+
+export const useRecords = () => {
+  const [records, setRecords] = useState<RecordItem[]>([]);
+  useEffect(() => {
+    setRecords(JSON.parse(window.localStorage.getItem("records") || "[]"));
+  }, []);
+  useUpdate(() => {
+    window.localStorage.setItem("records", JSON.stringify(records));
+  }, [records]);
+
+  const addRecord = (newRecord: newRecordItem) => {
+    if (newRecord.amount <= 0) {
+      Toast.show("请输入金额");
+      return false;
+    }
+    if (newRecord.tagIds.length === 0) {
+      Toast.show("请选择标签");
+      return false;
+    }
+    const record = { ...newRecord, createdAt: new Date().toISOString() };
+    setRecords([...records, record]);
+    return true;
+  };
+  return { records, addRecord };
+};
